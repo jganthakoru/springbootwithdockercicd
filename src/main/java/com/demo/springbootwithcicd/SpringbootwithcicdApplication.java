4a7e568f70a442d498aa1dacd2b690e2c14fc98d
jganthakoru <94169425+jganthakoru@users.noreply.github.com>
2024-01-05 16:56:26 +0530

    Update SpringbootwithcicdApplication.java
    second commit

--- src/main/java/com/demo/springbootwithcicd/SpringbootwithcicdApplication.java
+++ src/main/java/com/demo/springbootwithcicd/SpringbootwithcicdApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootwithcicdApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootwithcicdApplication.class, args)
+		SpringApplication.run(SpringbootwithcicdApplication.class, args);
 	}
 
 }
